d89e88b69b24f54f6ffe6a52b15df77562aee090
abdel <abdelmounaim.lallouache@gmail.com>
2019-05-17 21:50:47 -0500

    selecting caracters functions

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,30 +1,65 @@
-var caracter;
 
+$( document ).ready(function() {
+
+var caracter;
+var picked = true;
+var enemy;
+var defenderarea = true;
 
 
 $("#fighter1").click(function() {
-    caracter = 1;
-    pickCaracter(caracter);
-    // console.log($("#fighter1").attr('id'));
+        caracter = "fighter1"; 
+        pickCaracter(caracter, picked);
+        picked = false;
+        if (($(".choosenone").attr("id")) !== "fighter1" && defenderarea){
+            enemy= $("#fighter1");
+            pickEnemy(enemy);
+            defenderarea = false;
+
+        }
 });
 $("#fighter2").click(function() {
-    caracter = 2;
-    pickCaracter(caracter);
+    caracter = "fighter2";
+        pickCaracter(caracter, picked);
+        picked = false;
+        if (($(".choosenone").attr("id")) !== "fighter2" && defenderarea){
+            enemy= $("#fighter2");
+            pickEnemy(enemy);
+            defenderarea = false;
+
+        }
+        
 });
 $("#fighter3").click(function() {
-    caracter = 3;
-    pickCaracter(caracter);
+    caracter = "fighter3";
+        pickCaracter(caracter, picked);
+        picked = false;
+        if (($(".choosenone").attr("id")) !== "fighter3" && defenderarea){
+            enemy= $("#fighter3");
+            pickEnemy(enemy);
+            defenderarea = false;
+
+        }
 });
 $("#fighter4").click(function() {
-    caracter = 4;
-    pickCaracter(caracter);
+    caracter = "fighter4";
+        pickCaracter(caracter, picked);
+        picked = false;
+        if (($(".choosenone").attr("id")) !== "fighter4" && defenderarea){
+            enemy= $("#fighter4");
+            pickEnemy(enemy);
+            defenderarea = false;
+
+        }
 });
 
 
 
-function pickCaracter(c){
- if(c == 1 ){
+function pickCaracter(c , p){
+if (p){
+ if(c == "fighter1" ){
     $("#thirdrow").after($("#fighter1"));
+    $("#fighter1").attr("class","col-md-auto choosenone");
     $("#fifthrow").append($("#fighter2"));
     $("#fighter2").attr("class","col-md-auto enemiesToFight");
     $("#fifthrow").append($("#fighter3"));
@@ -32,19 +67,23 @@ function pickCaracter(c){
     $("#fifthrow").append($("#fighter4"));
     $("#fighter4").attr("class","col-md-auto enemiesToFight");
     $("#firstrow").empty();
+    p = false;
  }
- if(c == 2 ){
+ if(c == "fighter2" ){
     $("#thirdrow").after($("#fighter2"))
+    $("#fighter2").attr("class","col-md-auto choosenone");
     $("#fifthrow").append($("#fighter1"));
-    $("#fighter1").attr("class","col-md-auto enemiesToFight");
+    $("#fighter1").attr("class","enemiesToFight col-md-auto ");
     $("#fifthrow").append($("#fighter3"));
-    $("#fighter3").attr("class","col-md-auto enemiesToFight");
+    $("#fighter3").attr("class","enemiesToFight col-md-auto ");
     $("#fifthrow").append($("#fighter4"));
-    $("#fighter4").attr("class","col-md-auto enemiesToFight");
+    $("#fighter4").attr("class","enemiesToFight col-md-auto ");
     $("#firstrow").empty();
+    p = false;
  }
- if(c == 3 ){
+ if(c == "fighter3" ){
     $("#thirdrow").after($("#fighter3"))
+    $("#fighter3").attr("class","col-md-auto choosenone");
     $("#fifthrow").append($("#fighter1"));
     $("#fighter1").attr("class","col-md-auto enemiesToFight");
     $("#fifthrow").append($("#fighter2"));
@@ -52,9 +91,11 @@ function pickCaracter(c){
     $("#fifthrow").append($("#fighter4"));
     $("#fighter4").attr("class","col-md-auto enemiesToFight");
     $("#firstrow").empty();
+    p = false;
  }
- if(c == 4 ){
+ if(c == "fighter4"){
     $("#thirdrow").after($("#fighter4"))
+    $("#fighter4").attr("class","col-md-auto choosenone");
     $("#fifthrow").append($("#fighter1"));
     $("#fighter1").attr("class","col-md-auto enemiesToFight");
     $("#fifthrow").append($("#fighter2"));
@@ -62,10 +103,16 @@ function pickCaracter(c){
     $("#fifthrow").append($("#fighter3"));
     $("#fighter3").attr("class","col-md-auto enemiesToFight");
     $("#firstrow").empty();
+    p = false;
  }
 }
+}
 
-function pickEnemy(){
+function pickEnemy(e){
+    $("#ninethrow").append(e);
+    e.attr("class","col-md-auto defender");
 
 }
 
+
+});
